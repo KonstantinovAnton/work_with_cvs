861f86bf3dc82aff3bcde463a18475fb611a65f9
Konstantinov <akonstantinov.2003.sokol@gmail.com>
2022-10-09 21:23:57 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace work_with_cvs
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            
+
+            try
+            {
+                List<workWithCsv.lenghtValues> lv = new List<workWithCsv.lenghtValues>();
+                workWithCsv.getData("dataLenght.csv", lv);
+                List<workWithCsv.lenghtValues> newLV = calcAllValues(lv);
+                workWithCsv.inputData("dataLenght.csv", newLV);
+
+                Console.WriteLine("Длины успешно посчитаны и добавлены в файл"); 
+            }
+            catch
+            {
+                Console.WriteLine("Поймано исключение");
+            }
+            Console.ReadKey();
+        }
+    
+        static List<workWithCsv.lenghtValues> calcAllValues(List<workWithCsv.lenghtValues> L)
+        {
+            List<workWithCsv.lenghtValues> newLV = new List<workWithCsv.lenghtValues>();
+
+            foreach (workWithCsv.lenghtValues item in L)
+            {
+                newLV.Add(calcRowValues(item));
+            }
+
+            return newLV;
+        }
+
+        static workWithCsv.lenghtValues calcRowValues(workWithCsv.lenghtValues lv)
+        {
+           switch(findNotNull(lv))
+            {
+                case 0:
+                    lv.m = 0;
+                    lv.miles = lv.km  * 0.621371;
+                    lv.feet = lv.km * 3280.84;
+                    lv.yards = lv.km * 1093.61;
+                    lv.inches = lv.km * 39370.1;
+                    lv.versts = lv.km * 0.937383;
+                    break;
+                case 1:
+                    lv.km = 0;
+                    lv.miles = lv.m / 1000 * 0.621371;
+                    lv.feet = lv.m  / 1000 * 3280.84;
+                    lv.yards = lv.m / 1000 * 1093.61;
+                    lv.inches = lv.m / 1000* 39370.1;
+                    lv.versts = lv.m /1000 *0.937383;
+                    break;
+                case 2:
+                    lv.km = (int)(lv.miles * 1.60934) / 1;
+                    lv.m = (int)(((lv.miles * 1.60934) - lv.km) * 1000);
+                    lv.feet = (lv.km + lv.m / 1000) * 3280.84;
+                    lv.yards = (lv.km + lv.m / 1000) * 1093.61;
+                    lv.inches = (lv.km + lv.m / 1000) * 39370.1;
+                    lv.versts = (lv.km + lv.m / 1000) * 0.937383;
+                    break;
+
+                case 3:
+                    lv.km = (int)(lv.feet * 0.0003048) / 1;
+                    lv.m = (int)(lv.feet * 0.3048) / 1;
+                    lv.miles = lv.m * 0.000621371;
+                    lv.yards = lv.m / 1000 * 1093.61;
+                    lv.inches = lv.m / 1000 * 39370.1;
+                    lv.versts = lv.m /1000 *0.937383;
+                    break;
+
+                case 4:
+                    lv.km = (int)(lv.yards * 0.0009144) / 1;
+                    lv.m = (int)((lv.yards * 0.0009144) - lv.km) * 1000;
+                    lv.miles = lv.km * 0.621371;
+                    lv.feet = lv.km * 3280.84;
+                    lv.inches = lv.km * 39370.1;
+                    lv.versts = lv.km * 0.937383;
+                    break;
+
+                case 5:
+                    lv.km = (int)(lv.inches * 0.0000254) / 1;
+                    lv.m = (int)((lv.inches * 0.0254));
+                    lv.miles = lv.m / 1000 * 0.621371;
+                    lv.feet = lv.m /1000 * 3280.84;
+                    lv.yards = lv.m / 1000* 1093.61;
+                    lv.versts = lv.m / 1000 * 0.937383;
+                    break;
+                case 6:
+                    lv.km = (int)(lv.versts * 1.0668) / 1;
+                    lv.m = (int)(((lv.versts * 1.0668) - lv.km) * 1000);
+                    lv.inches = (lv.km  + lv.m/ 1000) * 39370.1;
+                    lv.miles = (lv.km + lv.m / 1000) * 0.621371;
+                    lv.feet = (lv.km + lv.m / 1000) * 3280.84;
+                    lv.yards = (lv.km + lv.m / 1000) * 1093.61;
+                    break;
+                case 10:
+                    lv.miles = (lv.km + (lv.m * 0.001)) * 0.621371;
+                    lv.feet = (lv.km + (lv.m * 0.001)) * 3280.84;
+                    lv.yards = (lv.km + (lv.m * 0.001)) * 1093.61;
+                    lv.inches = (lv.km + (lv.m * 0.001)) * 39370.1;
+                    lv.versts = (lv.km + (lv.m * 0.001)) * 0.937383;
+                    break;
+                case -1:
+                    lv.m = -1;
+                    lv.km = -1;
+                    lv.miles = -1;
+                    lv.feet = -1;
+                    lv.yards = -1;
+                    lv.inches = -1;
+                    lv.versts = -1;
+                    break;
+            }
+
+            return lv;
+        }
+
+        static int findNotNull(workWithCsv.lenghtValues lv)
+        {
+            if (lv.m != 0 && lv.km != 0) return 10;
+            if (lv.km != 0) return 0;
+            if (lv.m != 0) return 1;
+            if (lv.miles != 0) return 2;
+            if (lv.feet != 0) return 3;
+            if (lv.yards != 0) return 4;
+            if (lv.inches != 0) return 5;
+            if (lv.versts != 0) return 6;
+
+            return - 1;
+        }
+    }
+}
---
+++ workWithCsv.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace work_with_cvs
+{
+    public class workWithCsv
+    {
+       public struct lenghtValues
+        {
+            public double km, m, miles, feet, yards, inches, versts;
+
+            public string concat()
+            {
+                return km.ToString() + ";" + m.ToString() + ";" + miles.ToString() + ";" + feet.ToString() + ";" + yards.ToString() + ";" + inches.ToString() + ";" + versts.ToString();
+            }
+        }
+
+
+        // Чтение записей
+      public static void getData(string path, List<lenghtValues> L)
+        {
+            int count = -1;
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                while (sr.EndOfStream != true)
+                {
+
+                    count++;
+                    if (count == 0)
+                    {
+                        string[] arrayPsevdo = sr.ReadLine().Split(';');
+                        continue;
+                    }
+
+                    string[] array = sr.ReadLine().Split(';');
+
+
+                    for (int i = 0; i < array.Length; i++)
+                    {
+                        try
+                        {
+                            array[i] = array[i].Replace(',', '.');
+                            Convert.ToDouble(array[i]);
+                        }
+                        catch
+                        {
+                            array[i] = "0";
+                        }
+                    }
+
+                    
+                    L.Add(new lenghtValues()
+                    {
+                        km = Convert.ToDouble(array[0]),
+                        m = Convert.ToDouble(array[1]),
+                        miles = Convert.ToDouble(array[2]),
+                        feet = Convert.ToDouble(array[3]),
+                        yards = Convert.ToDouble(array[4]),
+                        inches = Convert.ToDouble(array[5]),
+                        versts = Convert.ToDouble(array[6])
+                    });
+                }
+            }
+        }
+
+    
+
+        // Вставка записей
+       public static void inputData(string path, List<lenghtValues> L)
+        {
+            using (StreamWriter sw = new StreamWriter(path, true, Encoding.Default))
+            {
+
+               sw.WriteLine("Километры;Метры;Мили;Футы;Ярды;Дюймы;Версты");
+                foreach (lenghtValues item in L)
+                {
+                    sw.WriteLine(item.concat());
+                }
+            }
+        }
+    }
+}
